f41f97796c39df0023f7b90464486db0e1630fa8
Tony van der Krogt <tony.vanderkrogt@student.hu.nl>
2020-04-23 20:28:58 +0200

    First start of the code

---
+++ src/Lexer.py
@@ -0,0 +1,16 @@
+class Tokens:
+
+    def __init__(self):
+        Tokens = {
+            "pf": "PLUS_ONE",
+            "bl": "MIN_ONE",
+            "b": "MOV_RIGHT",
+            "t": "MOV_LEFT",
+            "!": "OUTPUT",
+            "?": "INPUT",
+            "*gasp*": "OPEN_LOOP",
+            "*pomf*": "CLOSE_LOOP"
+        }
+
+    def __str__(self):
+        return 0
